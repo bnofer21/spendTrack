017ff0a333459d2126b08d386c69d6bbbf49bade
Yuriy <58361435+bnofer21@users.noreply.github.com>
2023-01-29 15:48:22 +0400

    New func loadMore

--- spendTrack/Main Module/Controller/MainController.swift
+++ spendTrack/Main Module/Controller/MainController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MainController: UIViewController {
     
+    var isLoading = false
+    var isLastTrans = false
     var transactions: [Transaction]
     var balance: Balance
     var mainView = MainView()
@@ -79,6 +81,26 @@ class MainController: UIViewController {
         }
     }
     
+    private func loadMoreData(startIndex: Int) {
+        if !isLoading, !isLastTrans {
+            DispatchQueue.global().asyncAfter(deadline: .now()+1) {
+                DataManager.shared.loadTransactions(startIndex: startIndex) { transactions in
+                    if transactions.count == 20 {
+                        self.isLoading.toggle()
+                    } else {
+                        self.isLastTrans = true
+                    }
+                    self.transactions.append(contentsOf: transactions)
+                    self.transactions.sort(by: { $0.date! > $1.date! })
+                }
+                DispatchQueue.main.async {
+                    self.mainView.transactionsTableView.reloadData()
+                    self.isLoading = false
+                }
+            }
+        }
+    }
+    
     @objc func newSpending() {
         let vc = NewSpendController(balance: balance)
         navigationController?.pushViewController(vc, animated: true)
@@ -106,19 +128,58 @@ class MainController: UIViewController {
 
 extension MainController: UITableViewDelegate, UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if transactions.count > 10 {
+            return 2
+        } else {
+            return 1
+        }
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        transactions.count
+        if section == 0 {
+            return transactions.count
+        } else {
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: TransactionCell.id, for: indexPath) as? TransactionCell else { return UITableViewCell() }
-        cell.viewModel = TransactionViewModel(transaction: transactions[indexPath.row])
-        return cell
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TransactionCell.id, for: indexPath) as? TransactionCell else { return UITableViewCell() }
+            cell.viewModel = TransactionViewModel(transaction: transactions[indexPath.row])
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: LoadingCell.id, for: indexPath) as? LoadingCell, transactions.count > 10 else { return UITableViewCell() }
+            if isLastTrans {
+                cell.activityIndicator.isHidden = true
+            } else {
+                cell.isHidden = false
+                cell.activityIndicator.startAnimating()
+            }
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        if indexPath.row == transactions.count-1, !isLoading {
+            loadMoreData(startIndex: indexPath.row+1)
+            print("loaded more")
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        50
+        if indexPath.section == 0 {
+            return 50
+        } else {
+            if isLastTrans {
+                return 0
+            } else {
+                return 50
+            }
+        }
     }
     
 }
